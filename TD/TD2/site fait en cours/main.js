f6a08cb40acd885969b6b0683a1607066db06e75
Humbert Evan <75947702+Fifie38@users.noreply.github.com>
2022-10-14 09:29:41 +0200

    update td2
    js

--- TD/TD2/site fait en cours/main.js
+++ TD/TD2/site fait en cours/main.js
@@ -1,8 +1,8 @@
-var image = document.getElementById("img1");
+var image = document.querySelector("img");
 
 image.addEventListener("click", function(){
     var imgSrc = image.getAttribute('src')
-    if (!imgSrc === "./IMG.png"){
+    if (imgSrc === "./IMG.png"){
         image.setAttribute('src', "./img2.png")
     } else {
         image.setAttribute('src', './IMG.png' )
