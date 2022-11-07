76c0c506c5326c2e609a6449f73a8ad521095ba7
Humbert Evan <75947702+Fifie38@users.noreply.github.com>
2022-11-07 08:30:15 +0100

    update

--- TD/TD5/script_TD5.js
+++ TD/TD5/script_TD5.js
@@ -1,18 +1,34 @@
-var nb = document.getElementById("nombre");
+// var nb = document.getElementById("nombre");
 
-var bouton = document.getElementById("fact");
+// var bouton = document.getElementById("fact");
 
-var res = document.getElementById("resultat");
+// var res = document.getElementById("resultat");
 
-bouton.addEventListener("click", afficheRes);
-function afficheRes(){
-  res.innerHTML = facto(nb.value);
-}
+// bouton.addEventListener("click", afficheRes);
+// function afficheRes(){
+//   res.innerHTML = facto(nb.value);
+// }
 
-function facto(n){
-  var f = 1
-  for (var i=2; i<=n; i=i+1){
-	f = f*i;
-  }
-  return f;
+// function facto(n){
+//   var f = 1
+//   for (var i=2; i<=n; i=i+1){
+// 	f = f*i;
+//   }
+//   return f;
+// }
+
+const nb1 = document.getElementById("nombre1");
+const nb2 = document.getElementById("nombre2");
+const bouton = document.getElementById("moyenne");
+const res = document.getElementById("resultat");
+
+bouton.addEventListener("click", () => {
+    console.log((nb1.value, nb2.value));
+    res.innerHTML = moyenne(parseInt(nb1.value), parseInt(nb2.value));
+});
+
+function moyenne(_nb1, _nb2) {
+    f = 0;
+    f = (_nb1 + _nb2) / 2;
+    return f;
 }
