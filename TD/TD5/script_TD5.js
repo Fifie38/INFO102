07634f6057ea63eea5483a8a5607c8d61c3cf6bd
Humbert Evan <75947702+Fifie38@users.noreply.github.com>
2022-11-07 09:37:32 +0100

    qsdqsd

--- TD/TD5/script_TD5.js
+++ TD/TD5/script_TD5.js
@@ -17,18 +17,55 @@
 //   return f;
 // }
 
+// ------------------------------------------------------------------------------
+// Formulaire 2
 const nb1 = document.getElementById("nombre1");
 const nb2 = document.getElementById("nombre2");
 const bouton = document.getElementById("moyenne");
-const res = document.getElementById("resultat");
+const res2 = document.getElementById("resultat");
+// Formulaire 3
+const vitesse_vent = document.getElementById("vitesse-vent");
+const etat_mer = document.getElementById("etat_mer");
+const drapeau = document.getElementById("drapeau");
+const res3 = document.getElementById("resultat3");
+// Formulaire 4
+const ul = document.getElementById("menu");
 
+
+// -------------------------------------------------------------------------------
+// Formulaire 2
 bouton.addEventListener("click", () => {
     console.log((nb1.value, nb2.value));
-    res.innerHTML = moyenne(parseInt(nb1.value), parseInt(nb2.value));
+    res2.innerHTML = moyenne(parseInt(nb1.value), parseInt(nb2.value));
 });
 
 function moyenne(_nb1, _nb2) {
-    f = 0;
-    f = (_nb1 + _nb2) / 2;
-    return f;
+    return (_nb1 + _nb2) / 2;
 }
+
+// Formulaire 3
+drapeau.addEventListener("click", () => {
+    console.log(vitesse_vent.value);
+    console.log(etat_mer.value);
+
+    if (vitesse_vent.value === 'zero' && etat_mer.value === 'calme') {
+        res3.innerHTML = 'Vert';
+        res3.style.backgroundColor = "green";
+    } else if ((vitesse_vent.value === 'vingt' && etat_mer.value === 'calme') || (etat_mer.value === 'agitee' && vitesse_vent.value === 'vingt') || (etat_mer.value === 'agitee' && vitesse_vent.value === 'zero')) {
+        res3.style.backgroundColor = 'orange';
+        res3.innerHTML = 'Orange';
+
+    } else {
+        res3.style.backgroundColor = 'red';
+        res3.innerHTML = 'Rouge';
+
+    }
+});
+
+// Formulaire 4
+ul.addEventListener("click", (e) => {
+    const li = e.srcElement.innerHTML.replace(' ', '_');
+    console.log(li.replace(' ', '_'));
+
+    document.body.style.backgroundImage = "url('./images/'+'li.replace(' ', '_') '.jpg')";
+});
