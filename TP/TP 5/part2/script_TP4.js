d9244fb18583261f737ffb60fce33ccd25b2002b
Humbert Evan <75947702+Fifie38@users.noreply.github.com>
2022-11-25 15:26:42 +0100

    setup tp5 part 2

---
+++ TP/TP 5/part2/script_TP4.js
@@ -0,0 +1,34 @@
+var zone_dessin = document.getElementById("dessin");
+var ctx = zone_dessin.getContext('2d');
+
+var en_dessin = false;
+
+ctx.strokeStyle = "black";
+ctx.lineWidth = 2;
+
+zone_dessin.onmousedown = function(e){
+	
+}
+
+zone_dessin.onmousemove = function(e){
+	
+}
+
+zone_dessin.onmouseup = function(e){
+	
+};
+
+function dessiner(x,y){
+	
+}
+
+couleurs.onclick = function(e) {
+	
+}
+
+traits.onclick = function(e) {
+	var epaisseur = e.target.getAttribute("alt");
+	
+}
+
+
