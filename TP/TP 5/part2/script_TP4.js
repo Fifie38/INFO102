2021c0202d1ed552c5042c5ffd87637190c6fac7
Humbert Evan <75947702+Fifie38@users.noreply.github.com>
2022-11-25 16:20:10 +0100

    dds

--- TP/TP 5/part2/script_TP4.js
+++ TP/TP 5/part2/script_TP4.js
@@ -7,28 +7,38 @@ ctx.strokeStyle = "black";
 ctx.lineWidth = 2;
 
 zone_dessin.onmousedown = function(e){
-	
+	ctx.beginPath();
+	ctx.moveTo(e.offsetX, e.offsetY);
 }
 
 zone_dessin.onmousemove = function(e){
-	
+		let x = e.offsetX;
+		let y = e.offsetY;
+		dessiner(x, y);
 }
 
 zone_dessin.onmouseup = function(e){
-	
+	en_dessin = false;
+	ctx.stroke();
 };
 
 function dessiner(x,y){
-	
+	ctx.lineTo(x, y);
 }
 
 couleurs.onclick = function(e) {
-	
+	ctx.strokeStyle = e.target.getAttribute("alt");
 }
 
 traits.onclick = function(e) {
-	var epaisseur = e.target.getAttribute("alt");
-	
+	if (e.target.getAttribute("alt") == "taille 1"){
+		ctx.lineWidth = "3";
+	} else if (e.target.getAttribute("alt") == "taille 2"){
+		ctx.lineWidth = "2";
+	}
+	else {
+		ctx.lineWidth = "1";
+	}
 }
 
 
