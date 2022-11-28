caf35779492c3ae4a3bc77c01b1a4a6ee0a49a10
Humbert Evan <evan.humbert@outlook.com>
2022-11-28 15:59:33 +0100

    update

--- racine/TDs/TD3/main.js
+++ racine/TDs/TD3/main.js
@@ -10,3 +10,12 @@ mySpan.addEventListener("click", () => {
 gerard.addEventListener("click", ()=> {
     gerard.style.transform = "rotate(360deg)";
 })
+// Created by : https://github.com/Fifie38
+//   ______ _____ ______ _____ ______ ____   ___  
+//  |  ____|_   _|  ____|_   _|  ____|___ \ / _ \
+//  | |__    | | | |__    | | | |__    __) | (_) |
+//  |  __|   | | |  __|   | | |  __|  |__ < > _ < 
+//  | |     _| |_| |     _| |_| |____ ___) | (_) |
+//  |_|    |_____|_|    |_____|______|____/ \___/ 
+                                               
+//    Created by : https://github.com/Fifie38
--- racine/TDs/TD5/script_TD5.js
+++ racine/TDs/TD5/script_TD5.js
@@ -1,3 +1,13 @@
+// Created by : https://github.com/Fifie38
+//   ______ _____ ______ _____ ______ ____   ___  
+//  |  ____|_   _|  ____|_   _|  ____|___ \ / _ \
+//  | |__    | | | |__    | | | |__    __) | (_) |
+//  |  __|   | | |  __|   | | |  __|  |__ < > _ < 
+//  | |     _| |_| |     _| |_| |____ ___) | (_) |
+//  |_|    |_____|_|    |_____|______|____/ \___/ 
+                                               
+//    Created by : https://github.com/Fifie38
+
 // var nb = document.getElementById("nombre");
 
 // var bouton = document.getElementById("fact");
